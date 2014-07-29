1b492d5e3d60e495565e551da23b1f8a72a3b72c
Craig Nakamoto <craig.nakamoto@claero.com>
2014-07-29 19:25:26 -0400

    defaulting status to 0 and other tweaks

--- classes/Controller/CL4/Ajax.php
+++ classes/Controller/CL4/Ajax.php
@@ -6,7 +6,7 @@ class Controller_CL4_Ajax extends Controller_Base {
 	public $cascade_sql = FALSE;
 	public $output = NULL;
 	public $output_type = 'html'; // 'html', 'json', or 'xml' ?\
-	public $status = 1;
+	public $status = 0;
 	public $html = 'no html specified';
 
 	public function before() {
@@ -17,10 +17,7 @@ class Controller_CL4_Ajax extends Controller_Base {
 		$this->record_limit = CL4::get_param('limit', 10);
 		$this->output_type = CL4::get_param('type', 'json'); // 'html', 'json', or 'xml' ?
 
-		$this->output = array(
-			'html' => '',
-			'status' => 1,
-		);
+		$this->output = array();
 
 		parent::before();
 	}
@@ -76,6 +73,7 @@ class Controller_CL4_Ajax extends Controller_Base {
 			);
 		}
 		$this->html = $jason_result;
+		$this->status = 1;
 		$this->action_after();
 	}
 
@@ -95,21 +93,55 @@ class Controller_CL4_Ajax extends Controller_Base {
 	 * get / set a user or company setting via ajax
 	 */
 	public function action_get_smart_parameter() {
-		$parameter_name = CL4::get_param('parameter_name', NULL);
-		$default = CL4::get_param('default', NULL);
-		$type = CL4::get_param('type', 'user');
-		// todo: security check!!!
-		if ( ! empty($parameter_name)) {
-			try {
-				$this->html = Base::get_smart_parameter($parameter_name, $default, $type);
-				$this->status = 1;
-			} catch (exception $e) {
-				$this->html = 'failed to get smart parameter: ' . Kohana_Exception::text($e);
-				$this->status = 0;
+		if (Auth::instance()->logged_in()) {
+			$parameter_name = CL4::get_param('parameter_name', NULL);
+			$default = CL4::get_param('default', NULL);
+			$type = CL4::get_param('type', 'user');
+			// todo: security check!!!
+			if ( ! empty($parameter_name)) {
+				try {
+					$this->html = Base::get_smart_parameter($parameter_name, $default, $type);
+					$this->status = 1;
+				} catch (exception $e) {
+					$this->html = 'failed to get smart parameter: ' . Kohana_Exception::text($e);
+				}
+			} else {
+				$this->html = 'failed to get smart parameter, no parameter name specified';
 			}
-		} else {
-			$this->html = 'failed to get smart parameter, no parameter name specified';
+		}
+		$this->action_after();
+	}
+
+	public function action_toggle_flag() {
+		if (Auth::instance()->logged_in()) {
+			$object_name = CL4::get_param('object_name', NULL);
+			$column_name = CL4::get_param('column_name', NULL);
+			$row_id = Kohana_Request::$current->param('id');
+
 			$this->status = 0;
+
+			if (in_array($object_name, array('Private_Order_Qc', 'Private_Order_On', 'Private_Order_Inventory'))) {
+				$object = ORM::factory($object_name, $row_id);
+				if ($object->loaded()) {
+					if (in_array($column_name, array('email_sent_flag', 'saq_conf_flag', 'pick_sent_flag', 'invoiced_flag'))) {
+						// toggle the column value
+						$object->$column_name = ($object->$column_name == 1) ? 0 : 1;
+						if ($object->save() !== false) {
+							$this->html = "This action has been completed.";
+							$this->output['value'] = $object->$column_name;
+							$this->status = 1;
+						} else {
+							$this->html = "failed to save the object in ajax toggle_flag() : {$object_name} {$column_name} {$row_id}";
+						}
+					} else {
+						$this->html = 'invalid column name in ajax toggle_flag(), please add ' . $column_name . ' to the list of allowed columns';
+					}
+				} else {
+					$this->html = "failed to load the object in ajax toggle_flag() : {$object_name} {$column_name} {$row_id}";
+				}
+			} else {
+				$this->html = 'invalid object name in ajax toggle_flag(), please add ' . $object_name . ' to the list of allowed objects';
+			}
 		}
 		$this->action_after();
 	}
